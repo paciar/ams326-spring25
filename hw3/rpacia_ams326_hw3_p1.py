35153fd8c1d669cea4ffa3e6da1dbcd9ba1e8e71
paciar <paciar04@gmail.com>
2025-04-15 23:52:30 -0400

    Plotted probabilities

--- hw3/rpacia_ams326_hw3_p1.py
+++ hw3/rpacia_ams326_hw3_p1.py
@@ -16,6 +16,8 @@ Buffon Disc Problem
 
 import numpy as np
 import math
+import matplotlib.pyplot as plot
+import matplotlib.patches as mpatches
 
 def buffon_disc(d, n):
     # I'll use a 20 by 20 grid for point generation, but the "lines" can extend infinitely (i.e. discs generated on the border of the grid can still cross lines that are outside of the grid)
@@ -43,13 +45,47 @@ def buffon_disc(d, n):
 def main():
     n = 4_444_444
     diameters = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.5, 2, 3]
-
+    probs = {}
     for d in diameters:
         crossings, probabilities = buffon_disc(d, n)
         print(f"Diameter: {d}")
-        print(f"Crossings: {crossings}")
-        print(f"Probabiltiies: {probabilities}")
+        for i in range(len(crossings)):
+            if (i == 0):
+                print(f"Number of discs crossing at least {i+1} line: {crossings[i]}")
+            else:
+                print(f"Number of discs crossing at least {i+1} lines: {crossings[i]}")
+        for i in range(len(probabilities)):
+            if (i == 0):
+                print(f"P[disc crossing at least {i+1} line]: {probabilities[i]:.4f}")
+            else:
+                print(f"P[disc crossing at least {i+1} lines]: {probabilities[i]:.4f}")
+        probs[d] = probabilities
         print()
+
+    # Referenced from https://matplotlib.org/stable/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
+    # Refined with ChatGPT
+    diameter_labels = [f"d={d}" for d in diameters]
+    prob = np.array([probs[d] for d in diameters]).T
+    x = np.arange(len(diameters))*5
+    width = 1
+    fig, ax = plot.subplots(figsize=(20,20), layout="constrained")
+    for i, probability in enumerate(prob):
+        offset = width * i
+        rects = ax.bar(x + offset, probability, width)
+        ax.bar_label(rects, padding=10, fmt="%.2f", fontsize=6)
+    ax.set_ylabel('Probability')
+    ax.set_xlabel('Diameter')
+    ax.set_title('Buffon Disc: Probability of Crossing at least 1/2/3/4 Lines by Diameter')
+    ax.set_xticks(x + width * 1.5, diameter_labels)
+    ax.set_ylim(0, 1.1)
+    custom_legend = [
+        mpatches.Patch(color='C0', label='probability of crossing at least 1 line'),
+        mpatches.Patch(color='C1', label='probability of crossing at least 2 lines'),
+        mpatches.Patch(color='C2', label='probability of crossing at least 3 lines'),
+        mpatches.Patch(color='C3', label='probability of crossing at least 4 lines'),
+    ]
+    ax.legend(handles=custom_legend, loc='upper left', fontsize=8)
+    plot.show()
 
 if __name__ == "__main__":
     main()
