96d7735c96917b32642cb68e1c19d766dad9549d
paciar <paciar04@gmail.com>
2025-04-17 01:12:47 -0400

    Finalized Problem 3.1

--- hw3/rpacia_ams326_hw3_p1.py
+++ hw3/rpacia_ams326_hw3_p1.py
@@ -5,16 +5,16 @@
 
 """
 Buffon Disc Problem
-- Generate points to serve as the center of each circle
+- Randomly generate points (within a certain range) using a uniform distribution to serve as the center of each disc
 - Number of lines crossed can be calculated by calculating how many integers fall between the range of [center_x - d/2, center_x + d/2]
-- Lines will be placed at x = -2, -1, 0, 1, 2, etc. (integer x-values)
-- Simulate for all diameters d and keep counters for how many lines were crossed (probably using a dict) -- note that if a line crosses 2 lines it also crosses 1 line (the probabilities are calculated for "at least" 1, 2, 3, 4... lines)
-- Calculate probabilities using number of times n lines were crossed / total discs used for all n = 1, 2, 3, 4 lines
+- Lines will be placed at x = ..., -2, -1, 0, 1, 2, ... etc. (integer x-values)
+- Simulate for all diameters d and keep counters for how many lines were crossed (using a dict) -- note that if a line crosses at least 2 lines, it also crosses at least 1 line (the probabilities are calculated for "at least" 1, 2, 3, 4... lines in a cumulative fashion)
+- Calculate probabilities using (number of times at least n lines were crossed) / (total discs used) for all n = 1, 2, 3, 4 lines
 """
 
 import numpy as np
 import math
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 def buffon_disc(d, n):
@@ -26,12 +26,14 @@ def buffon_disc(d, n):
     r = d/2
     # For each point, check how many lines a disc centered at that point with diameter d it crosses
     # Maximum number of lines that can be crossed is 4 (maximum d value given in the problem is 3)
+    # crossings[i] = how many times at least (i+1) lines were crossed
     crossings = [0, 0, 0, 0]
     for center in disc_centers:
         # ChatGPT helped me with finding a formula to calculate crossings (i.e. how many integers occur within the range [x-r, x+r])
         center_x = center[0]
+        # Wrapped with max(0, ...) to guard against possible floating-point errors with floor/ceil
         num_crossings = max(0, math.floor(center_x + r) - math.ceil(center_x - r) + 1)
-        # We want to update the number of crossings "cumulatively" -- if a disc crossed 3 lines, it also crossed 2 lines and 1 line
+        # We want to update the number of crossings "cumulatively" -- if a disc crossed at least 3 lines, it also crossed at least 2 lines, and at least 1 line
         for i in range(num_crossings):
             crossings[i] += 1
     # Calculate probabilities
@@ -43,6 +45,7 @@ def buffon_disc(d, n):
 def main():
     n = 4_444_444
     diameters = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.5, 2, 3]
+    # Dict to map diameter to probabilities
     probs = {}
     for d in diameters:
         crossings, probabilities = buffon_disc(d, n)
@@ -66,7 +69,7 @@ def main():
     prob = np.array([probs[d] for d in diameters]).T
     x = np.arange(len(diameters))*5
     width = 1
-    fig, ax = plot.subplots(figsize=(20,20), layout="constrained")
+    fig, ax = plt.subplots(figsize=(20,20), layout="constrained")
     for i, probability in enumerate(prob):
         offset = width * i
         rects = ax.bar(x + offset, probability, width)
@@ -74,7 +77,7 @@ def main():
     ax.set_ylabel('Probability')
     ax.set_xlabel('Diameter')
     ax.set_title('Buffon Disc: Probability of Crossing at least 1/2/3/4 Lines by Diameter')
-    ax.set_xticks(x + width * 1.5, diameter_labels)
+    ax.set_xticks(x + width * 2, diameter_labels)
     ax.set_ylim(0, 1.1)
     custom_legend = [
         mpatches.Patch(color='C0', label='probability of crossing at least 1 line'),
@@ -83,7 +86,7 @@ def main():
         mpatches.Patch(color='C3', label='probability of crossing at least 4 lines'),
     ]
     ax.legend(handles=custom_legend, loc='upper left', fontsize=8)
-    plot.show()
+    plt.show()
 
 if __name__ == "__main__":
     main()
