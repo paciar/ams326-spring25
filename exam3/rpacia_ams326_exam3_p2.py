762b1581468bdc03b92aea2e0980eb3abe5ee856
paciar <paciar04@gmail.com>
2025-04-29 15:32:45 -0400

    Initial Exam 3 Commit

---
+++ exam3/rpacia_ams326_exam3_p2.py
@@ -0,0 +1,12 @@
+# AMS326 - Numerical Analysis, Spring 2025
+# Professor Yuefan Deng
+# Exam 3, Problem 3.2
+# Ralf Pacia
+
+import numpy as np
+
+def main():
+    exit(0)
+
+if __name__ == "__main__":
+    main()
